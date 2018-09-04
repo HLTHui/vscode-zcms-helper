3e692d89c945d1647f05e046ea0ca17a4a22cfde
wangzhaohui <alex.wang@zving.com>
2018-09-04 16:40:10 +0800

    完善提示数据，加图标

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # vscode-zcms-helper
 
-> A vscode extension for write ZCMS template  
-为ZCMS模板编写提供标签提示的VSCode插件。  
+> 为ZCMS模板编写提供标签提示的VSCode插件。
 
 ## 特性
 
-标签与标签属性的提示  
+ZCMS模板标签与标签属性的提示  
 
 * 每个标签与属性的提示  
 
-* 部分属性会提示值，例如 Boolean 类型的属性  
+* 部分属性会列举值，例如 Boolean 类型的属性  
 
 ### 引号与缩进配置
 
@@ -18,9 +17,9 @@
 2 - 引号与缩进配置  
 ```javascript
   "vscode-zcms-helper.indent-size": 2,
-  "vscode-zcms-helper.quotes": "double"    // double 或 single
+  "vscode-zcms-helper.quotes": "single"    // double 或 single，默认为 double
 ```
 
 ## Issues
-您的反馈让我们将 vscode-zcms-helper 改进的更好，提交 Issues 网址，  
+您的反馈让我们将 vscode-zcms-helper 改进的更好，请到以下地址提交 Issue：    
 https://github.com/abeet/vscode-zcms-helper/issues
--- out/app.js
+++ out/app.js
@@ -5,9 +5,10 @@ const ATTRS = require("./template-attributes.json");
 
 class ElementCompletionItemProvider {
   constructor() {
-      this.tagReg = /<([\w-]+)\s+/g;
+      this.tagReg = /<([\w\:-]+)\s+/g;
       this.attrReg = /(?:\(|\s*)(\w+)=['"][^'"]*/;
       this.tagStartReg = /<([\w-]*)$/;
+      this.zTagStartReg = /<([\w]+\:[\w-]*)$/;
   }
   getPreTag() {
       let line = this._position.line;
@@ -41,7 +42,7 @@ class ElementCompletionItemProvider {
   matchTag(reg, txt, line) {
       let match;
       let arr = [];
-      if (/<\/?[-\w]+[^<>]*>[\s\w]*<?\s*[\w-]*$/.test(txt) || (this._position.line === line && (/^\s*[^<]+\s*>[^<\/>]*$/.test(txt) || /[^<>]*<$/.test(txt[txt.length - 1])))) {
+      if (/<\/?[\w\:-]+[^<>]*>[\s\w]*<?\s*[\w\:-]*$/.test(txt) || (this._position.line === line && (/^\s*[^<]+\s*>[^<\/>]*$/.test(txt) || /[^<>]*<$/.test(txt[txt.length - 1])))) {
           return 'break';
       }
       while ((match = reg.exec(txt))) {
@@ -57,11 +58,11 @@ class ElementCompletionItemProvider {
       var range = new vscode_1.Range(start, position);
       return this._document.getText(range);
   }
-  getTagSuggestion() {
+  getTagSuggestion(noFirstPrefix) {
       let suggestions = [];
       let id = 100;
       for (let tag in TAGS) {
-          suggestions.push(this.buildTagSuggestion(tag, TAGS[tag], id));
+          suggestions.push(this.buildTagSuggestion(tag, TAGS[tag], id, noFirstPrefix));
           id++;
       }
       return suggestions;
@@ -82,35 +83,32 @@ class ElementCompletionItemProvider {
       let tagAttrs = this.getTagAttrs(tag);
       let preText = this.getTextBeforePosition(this._position);
       let prefix = preText.replace(/['"]([^'"]*)['"]$/, '').split(/\s|\(+/).pop();
-      // method attribute
-      const method = prefix[0] === '@';
-      // bind attribute
-      const bind = prefix[0] === ':';
+
       prefix = prefix.replace(/[:@]/, '');
-      if (/[^@:a-zA-z\s]/.test(prefix[0])) {
+      if (/[^@:a-zA-Z\s]/.test(prefix[0])) {
           return suggestions;
       }
       tagAttrs.forEach(attr => {
           const attrItem = this.getAttrItem(tag, attr);
           if (attrItem && (!prefix.trim() || this.firstCharsEqual(attr, prefix))) {
-              const sug = this.buildAttrSuggestion({ attr, tag, bind, method }, attrItem);
+              const sug = this.buildAttrSuggestion({ attr, tag }, attrItem);
               sug && suggestions.push(sug);
           }
       });
       for (let attr in ATTRS) {
           const attrItem = this.getAttrItem(tag, attr);
           if (attrItem && attrItem.global && (!prefix.trim() || this.firstCharsEqual(attr, prefix))) {
-              const sug = this.buildAttrSuggestion({ attr, tag: null, bind, method }, attrItem);
+              const sug = this.buildAttrSuggestion({ attr, tag: null }, attrItem);
               sug && suggestions.push(sug);
           }
       }
       return suggestions;
   }
-  buildTagSuggestion(tag, tagVal, id) {
+  buildTagSuggestion(tag, tagVal, id, noFirstPrefix) {
       const snippets = [];
       let index = 0;
       let that = this;
-      function build(tag, { subtags, defaults }, snippets) {
+      function build(tag, { subtags, defaults }, snippets, noFirstPrefix) {
           let attrs = '';
           defaults && defaults.forEach((item, i) => {
               attrs += ` ${item}=${that.quotes}$${index + i + 1}${that.quotes}`;
@@ -120,30 +118,29 @@ class ElementCompletionItemProvider {
           subtags && subtags.forEach(item => build(item, TAGS[item], snippets));
           snippets.push(`</${tag}>`);
       }
-      ;
       build(tag, tagVal, snippets);
+      let insertTextValue = prettyHTML('<' + snippets.join(''), { indent_size: this.size }).substr(1)
+      if(noFirstPrefix && tag.indexOf(':') !== -1){
+        // 为了修正 VSCode 中自动完成的bug，会导致tagName中:前面的部分被重复
+        insertTextValue = insertTextValue.substr(insertTextValue.indexOf(':')+1)
+      }      
       return {
           label: tag,
           sortText: `0${id}${tag}`,
-          insertText: new vscode_1.SnippetString(prettyHTML('<' + snippets.join(''), { indent_size: this.size }).substr(1)),
+          insertText: new vscode_1.SnippetString(insertTextValue),
           kind: vscode_1.CompletionItemKind.Snippet,
-          detail: `element-ui ${tagVal.version ? `(version: ${tagVal.version})` : ''}`,
+          detail: `ZCMS ${tagVal.version ? `(version: ${tagVal.version})` : ''}`,
           documentation: tagVal.description
       };
   }
-  buildAttrSuggestion({ attr, tag, bind, method }, { description, type, version }) {
-      if ((method && type === "method") || (bind && type !== "method") || (!method && !bind)) {
-          return {
-              label: attr,
-              insertText: (type && (type === 'flag')) ? `${attr} ` : new vscode_1.SnippetString(`${attr}=${this.quotes}$1${this.quotes}$0`),
-              kind: (type && (type === 'method')) ? vscode_1.CompletionItemKind.Method : vscode_1.CompletionItemKind.Property,
-              detail: tag ? `<${tag}> ${version ? `(version: ${version})` : ''}` : `element-ui ${version ? `(version: ${version})` : ''}`,
-              documentation: description
-          };
-      }
-      else {
-          return;
-      }
+  buildAttrSuggestion({ attr, tag }, { description, type, version }) {
+    return {
+        label: attr,
+        insertText: (type && (type === 'flag')) ? `${attr} ` : new vscode_1.SnippetString(`${attr}=${this.quotes}$1${this.quotes}$0`),
+        kind: (type && (type === 'method')) ? vscode_1.CompletionItemKind.Method : vscode_1.CompletionItemKind.Property,
+        detail: tag ? `<${tag}> ${version ? `(version: ${version})` : ''}` : `ZCMS ${version ? `(version: ${version})` : ''}`,
+        documentation: description
+    };
   }
   getAttrValues(tag, attr) {
       let attrItem = this.getAttrItem(tag, attr);
@@ -180,6 +177,10 @@ class ElementCompletionItemProvider {
       let txt = this.getTextBeforePosition(this._position);
       return this.tagStartReg.test(txt);
   }
+  isZTagStart() {
+      let txt = this.getTextBeforePosition(this._position);
+      return this.zTagStartReg.test(txt);
+  }
   firstCharsEqual(str1, str2) {
       if (str2 && str1) {
           return str1[0].toLowerCase() === str2[0].toLowerCase();
@@ -202,10 +203,11 @@ class ElementCompletionItemProvider {
       this._position = position;
       const config = vscode_1.workspace.getConfiguration('vscode-zcms-helper');
       this.size = config.get('indent-size');
-      const normalQuotes = config.get('quotes') === 'double' ? '"' : "'";
+      const normalQuotes = config.get('quotes') === 'single' ? "'" : '"';
       this.quotes = normalQuotes;
       let tag =  this.getPreTag();
       let attr = this.getPreAttr();
+      console.log(tag, this._position.character, this.isTagStart(), this.isZTagStart())
       if (this.isAttrValueStart(tag, attr)) {
           return this.getAttrValueSuggestion(tag.text, attr);
       }
@@ -219,6 +221,14 @@ class ElementCompletionItemProvider {
               case 'html':
                   // todo
                   return this.getTagSuggestion();
+          }
+      }else if(this.isZTagStart()) {
+          switch (document.languageId) {
+              case 'vue':
+                  return this.notInTemplate() ? [] : this.getTagSuggestion(true);
+              case 'html':
+                  // todo
+                  return this.getTagSuggestion(true);
           }
       }
       else {
